0e5e3cddfd7aa8104bebf7fc58991c0f8ca431df
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-03 23:43:58 +1100

    cannot add to fixed array

--- DataStructures/src/arrays/Main.java
+++ DataStructures/src/arrays/Main.java
@@ -22,16 +22,16 @@ public class Main {
 //
 		System.out.println("First Number is: " + firstNumber);
 		System.out.println("Third Number is: " + thirdNumber);
+
+//		 initialising an array to hold 3 integers
+		int[] numbers1 = new int[3];
+		numbers1[0] = 10;
+		numbers1[1] = 59;
+		numbers1[2] = 11;
+		System.out.println(numbers1.length); // prints 3
+
+		// adding one more integer
+		numbers1[3] = 594;
+		System.out.println(numbers1.length); // Error
 	}
 }
-
-// initialising an array to hold 3 integers
-//int[] numbers = new int[3];
-//numbers[0] = 10;
-//numbers[1] = 59;
-//numbers[2] = 11;
-//System.out.println(numbers.length); // prints 3
-//
-//// adding one more integer
-//numbers[3] = 594;
-//System.out.println(numbers.length); // Error
