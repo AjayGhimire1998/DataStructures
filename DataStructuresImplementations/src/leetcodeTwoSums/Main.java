a6b2d2710b253386098eea7a45e979a418556d68
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-06 00:01:08 +1100

    a leetcode final

--- DataStructuresImplementations/src/leetcodeTwoSums/Main.java
+++ DataStructuresImplementations/src/leetcodeTwoSums/Main.java
@@ -9,21 +9,15 @@ public class Main {
 	}
 
 	public static int[] twoSum(int[] nums, int target) {
-		int[] result = new int[2];
-		for (int i = 0; i < nums.length; i++) {
 
-			int current = nums[i];
-
-			for (int j = 1; j < nums.length; j++) {
-				if (current + nums[j] == target) {
-					result[0] = i;
-					System.out.println(result[0]);
-					result[1] = j;
-					System.out.println(result[1]);
+		for (int i = 1; i < nums.length; i++) {
+			for (int j = i; j < nums.length; j++) {
+				if (nums[j] + nums[j - i] == target) {
+					return new int[] { j - 1, j };
 				}
 			}
 		}
-		return result;
+		return null;
 	}
 
 }
