9cadb64f128389b0ce8f3f635e55f86f74387ba7
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-06 21:29:10 +1100

    reversing an array

--- DataStructuresImplementations/src/reverseAnArray/Main.java
+++ DataStructuresImplementations/src/reverseAnArray/Main.java
@@ -1,7 +1,29 @@
 package reverseAnArray;
 
+import java.util.Arrays;
+
 public class Main {
 	public static void main(String[] args) {
+		int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+		System.out.println(Arrays.toString(nums));
 
+		reverseArray(nums);
+		System.out.println(Arrays.toString(nums));
+
+	}
+
+	public static void reverseArray(int[] data) {
+		int temp;
+		for (int i = 0; i < data.length; i++) {
+			temp = data[i];
+			System.out.println("temp: " + temp);
+
+			data[i] = data[data.length - 1 - i];
+
+			System.out.println("data[i]: " + data[i]);
+
+			data[data.length - 1 - i] = temp;
+			System.out.println(data[data.length - 1 - i]);
+		}
 	}
 }
