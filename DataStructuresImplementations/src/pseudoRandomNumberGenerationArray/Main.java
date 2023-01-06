c1522d6ed7a6a46ec20cd0dcf42ee3e5804487c8
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-06 20:46:23 +1100

    trying to reverse an array

---
+++ DataStructuresImplementations/src/pseudoRandomNumberGenerationArray/Main.java
@@ -0,0 +1,9 @@
+package pseudoRandomNumberGenerationArray;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ DataStructuresImplementations/src/reverseAnArray/Main.java
@@ -0,0 +1,7 @@
+package reverseAnArray;
+
+public class Main {
+	public static void main(String[] args) {
+
+	}
+}
