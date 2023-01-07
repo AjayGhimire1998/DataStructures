8ded45756368e6b07505ac2440592f333f90ffdf
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-07 21:05:45 +1100

    searchInsertPosArray leetcode

---
+++ DataStructuresImplementations/Arrays/searchInsertPosArray/Main.java
@@ -0,0 +1,25 @@
+package searchInsertPosArray;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] nums = { 1, 3, 5, 6 };
+		System.out.println(searchInsert(nums, 7));
+	}
+
+	public static int searchInsert(int[] nums, int target) {
+		int count = 0;
+
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] == target) {
+				count = i;
+				break;
+			} else if (target > nums[i]) {
+				count++;
+			}
+
+		}
+		return count;
+	}
+
+}
