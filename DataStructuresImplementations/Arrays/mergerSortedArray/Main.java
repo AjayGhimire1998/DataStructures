14545ca38ad3c32600659f1502274786623300d7
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-07 23:24:27 +1100

    mergeSortedArray

---
+++ DataStructuresImplementations/Arrays/mergerSortedArray/Main.java
@@ -0,0 +1,36 @@
+package mergerSortedArray;
+
+import java.util.Arrays;
+
+public class Main {
+	public static void main(String[] args) {
+		int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+		int[] nums2 = { 2, 5, 6 };
+		int m = 3;
+		int n = 3;
+
+		merge(nums1, m, nums2, n);
+	}
+
+	public static void merge(int[] nums1, int m, int[] nums2, int n) {
+//		int current = 0;
+		// n = nums.length - m
+		for (int i = 0; i < n; i++) {
+			nums1[m] = nums2[i];
+			m++;
+
+		}
+
+		for (int j = 0; j < nums1.length; j++) {
+			int current = nums1[j];
+			int k = j;
+			while (k > 0 && nums1[k - 1] > current) {
+				nums1[k] = nums1[k - 1];
+				k--;
+			}
+			nums1[k] = current;
+		}
+
+		System.out.println(Arrays.toString(nums1));
+	}
+}
