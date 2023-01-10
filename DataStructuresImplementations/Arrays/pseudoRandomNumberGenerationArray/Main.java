e89e7624d455c568304518e83cf9bc507bf4ed56
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-10 21:28:38 +1100

    feeling demotivated

--- DataStructuresImplementations/Arrays/pseudoRandomNumberGenerationArray/Main.java
+++ DataStructuresImplementations/Arrays/pseudoRandomNumberGenerationArray/Main.java
@@ -1,9 +1,19 @@
 package pseudoRandomNumberGenerationArray;
 
+import java.util.Random;
+
 public class Main {
 
 	public static void main(String[] args) {
 
+	}
+
+	public static void randomNumberFormulaByJava(int a, int b, int n) {
+		int next = 0;
+
+		Random random = new Random();
+		random.setSeed((long) 100.0);
+		System.out.println(random.nextDouble());
 	}
 
 }
