b7dbc71cdf89982b6bb56f7af19c975500c5daa7
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-10 21:36:21 +1100

    random numbers arrays

--- DataStructuresImplementations/Arrays/pseudoRandomNumberGenerationArray/Main.java
+++ DataStructuresImplementations/Arrays/pseudoRandomNumberGenerationArray/Main.java
@@ -1,19 +1,26 @@
 package pseudoRandomNumberGenerationArray;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
 
 	public static void main(String[] args) {
-
-	}
-
-	public static void randomNumberFormulaByJava(int a, int b, int n) {
-		int next = 0;
-
+		int[] data = new int[10];
 		Random random = new Random();
-		random.setSeed((long) 100.0);
-		System.out.println(random.nextDouble());
+		random.setSeed(System.currentTimeMillis());
+
+		for (int i = 0; i < data.length; i++) {
+			data[i] = random.nextInt(100);
+		}
+		int[] original = Arrays.copyOf(data, data.length);
+		System.out.println("arrays equal before sort: " + Arrays.equals(data, original));
+		Arrays.sort(data);
+		System.out.println("arrays equal after sort: " + Arrays.equals(data, original));
+//		System.out.println(Arrays.asList(data).contains(25));
+		System.out.println("original: " + Arrays.toString(original));
+		System.out.println("data: " + Arrays.toString(data));
+
 	}
 
 }
