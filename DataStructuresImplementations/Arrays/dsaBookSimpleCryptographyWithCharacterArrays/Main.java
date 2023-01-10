104df26bd2051969529af2fabab2d33e996cbf6e
AjayGhimire <ajayghimire1998@gmail.com>
2023-01-10 22:08:35 +1100

    consistency is the key

--- DataStructuresImplementations/Arrays/dsaBookSimpleCryptographyWithCharacterArrays/Main.java
+++ DataStructuresImplementations/Arrays/dsaBookSimpleCryptographyWithCharacterArrays/Main.java
@@ -1,5 +1,33 @@
 package dsaBookSimpleCryptographyWithCharacterArrays;
 
+import java.util.Arrays;
+
 public class Main {
+	public static void main(String[] args) {
+		String name = "Ajay";
+		char[] nameChars = name.toCharArray(); // creates an array of characters of the string
+		System.out.println(Arrays.toString(nameChars));
+
+		System.out.println(createString(nameChars));
+//		System.out.println((char) (87));
+		System.out.println(Arrays.toString(createAlphabetsArray()));
+	}
+
+	public static String createString(char[] chars) { // creates s String from the characters array
+		String result = "";
+
+		for (int i = 0; i < chars.length; i++) {
+			result += chars[i];
+		}
+		return result;
+	}
+
+	public static char[] createAlphabetsArray() { // create a array of all alphabets characters
+		char[] alphabets = new char[26];
+		for (int i = 0; i < alphabets.length; i++) {
+			alphabets[i] = (char) ('a' + i);
+		}
+		return alphabets;
+	}
 
 }
