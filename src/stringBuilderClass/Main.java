2520328c8ac590c6fb110b727e1abfc036a94937
AjayGhimire <ajayghimire1998@gmail.com>
2023-02-26 17:39:03 +1100

    think Java first

---
+++ src/stringBuilderClass/Main.java
@@ -0,0 +1,25 @@
+package stringBuilderClass;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String title = "Data Structures and Algorithms in Java";
+		System.out.println();
+
+		String greeting = "Hello";
+		System.out.println(greeting);
+
+		greeting = "Hola";
+		System.out.println(greeting);
+
+		String hehe = new String("HEHEHEH");
+		System.out.println(hehe);
+
+		char[] myCharArray = { 'H', 'e', 'l', 'l', 'o', ',', ' ', 'W', 'o', 'r', 'l', 'd', '!' };
+		String myString = new String(myCharArray);
+		System.out.println(myString);
+		System.out.println(myString);
+
+	}
+
+}
